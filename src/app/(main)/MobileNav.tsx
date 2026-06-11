00521c898a6a0906c537f33fe3ac3720081253ef
Sverre Sundsdal <sundsdal@gmail.com>
2026-06-11 13:43:12 +0200

    Update app brand label

--- src/app/(main)/MobileNav.tsx
+++ src/app/(main)/MobileNav.tsx
@@ -78,7 +78,7 @@ export function MobileNav() {
       </MobileMenuButton>
       <Row alignItems="center" justifyContent="center" flexGrow={1}>
         <IconLabel icon={<Logo />} style={{ width: 'auto' }}>
-          <Text weight="bold">umami</Text>
+          <Text weight="bold">kin analytics GDPR</Text>
         </IconLabel>
       </Row>
     </Grid>
